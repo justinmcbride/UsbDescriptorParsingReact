f9535f94478470760e7e7711029d97ab81879e04
Justin McBride <justin@mcbride.io>
2020-08-06 01:22:09 -0600

    Initial support for video-specific class parsing

--- src/UsbDescriptors/DescriptorFactory.jsx
+++ src/UsbDescriptors/DescriptorFactory.jsx
@@ -18,7 +18,88 @@ class UsbBaseNode {
   }
 
   Print = () => {
-    console.log( `Base: rawData.length=${this.rawData.byteLength} bLength=${this.bLength()} bDescriptorType=[${this.bDescriptorType()}]` );
+    return `Unknown: rawData.length=[${this.rawData.byteLength}] bLength=[${this.bLength()}] bDescriptorType=[${this.bDescriptorType()}]`;
+  }
+  
+  PrintTreeFromHere = (tabLevel) => {
+    let formattedOutput = "";
+    const tabs = "  ".repeat(tabLevel);
+    formattedOutput += tabs;
+    formattedOutput += this.Print();
+    formattedOutput += "\n"
+
+    for( let child of this.children )
+    {
+      formattedOutput += child.PrintTreeFromHere(tabLevel + 1);
+    }
+
+    return formattedOutput;
+  }
+
+  ParseTree = ( rawData, interfaceClass, interfaceSubclass ) => {
+    const dataView = new Uint8Array( rawData );
+    for( let currentOffset = 0; currentOffset < dataView.length; ) {
+      // check size
+      if( currentOffset + 2 >= dataView.length ) {
+        // bad data here...
+        console.log( `Dangling data: currentOffset=[${currentOffset}] dataView.length=${dataView.length}` );
+        return;
+      }
+
+      const bLength = dataView[currentOffset + 0];
+      const bDescriptorType = dataView[currentOffset + 1];
+
+      console.log( `bLength=[${bLength}] bDescriptorType=[${bDescriptorType}]` );
+
+      const thisDescriptor = dataView.slice( currentOffset, currentOffset + bLength ); 
+      const newChildNode = NodeFactory( thisDescriptor );
+
+      // Some special cases
+      if( bDescriptorType === UsbConstants.DescriptorType.Configuration ) {
+        // at this point, we need to recursively add wTotalLength data under the configuration node.
+        // we also substract the length of the config descriptor itself from wTotalLength
+        const indexBegin = currentOffset + newChildNode.bLength();
+        const childDataLength = newChildNode.wTotalLength() - newChildNode.bLength();
+        const indexEnd = indexBegin + childDataLength;
+
+        const childData = rawData.slice( indexBegin, indexEnd );
+
+        newChildNode.ParseTree( childData, interfaceClass, interfaceSubclass );
+        currentOffset += childDataLength;
+      }
+      else if( bDescriptorType === UsbConstants.DescriptorType.Interface ) {
+        // further parsing needs to know the context of which usb class we're operating in
+        interfaceClass = newChildNode.bInterfaceClass();
+        interfaceSubclass = newChildNode.bInterfaceSubClass();
+        console.log( `New context: interfaceClass=[${interfaceClass}] interfaceSubclass=[${interfaceSubclass}]` );
+      }
+      else {
+        if( interfaceClass === UsbConstants.Uvc11.Class ) {
+          if( bDescriptorType === UsbConstants.Uvc11.DescriptorType.Interface ) {
+            if( interfaceSubclass === UsbConstants.Uvc11.Subclass.VideoControl ) {
+              console.log( `-->VC` );
+            }
+            else if( interfaceSubclass === UsbConstants.Uvc11.Subclass.VideoStreaming ) {
+              console.log( `-->VS` );
+            }
+          }
+        }
+      }
+
+      this.children.push( newChildNode );
+      currentOffset += bLength;
+    }
+  }
+};
+
+class RootNode extends UsbBaseNode {
+  constructor( rawData ) {
+    super( null );
+    this.ParseTree( rawData );
+  }
+
+  Print = () => {
+    return `Root`;
   }
 };
 
@@ -44,11 +125,72 @@ class DeviceDescriptor extends UsbBaseNode {
   }
 
   Print = () => {
-    console.log( `Device: verified=[${this.Verify()}] bLength=${this.bLength()} idVendor=[${this.idVendor()}] idProduct=[${this.idProduct()}]` );
+    return `Device: verified=[${this.Verify()}] bLength=[${this.bLength()}] idVendor=[${this.idVendor()}] idProduct=[${this.idProduct()}]`;
+  }
+};
+
+class ConfigurationDescriptor extends UsbBaseNode {
+  constructor( rawData ) {
+    super( rawData );
+  }
+
+  Verify = () => {
+    // TODO: verify structure is correct
+    if ( this.rawData.byteLength !== 9 ) return false;
+    return true;
+  }
+
+  wTotalLength = () => {
+    const dataView = new Uint16Array( this.rawData, 2, 1 );
+    return dataView[0];
+  }
+
+  Print = () => {
+    return `Configuration: verified=[${this.Verify()}] bLength=[${this.bLength()}] wTotalLength=[${this.wTotalLength()}]`;
   }
 }
 
-const NodeFactory = ( rawData, interfaceClass, subclass ) => {
+class InterfaceDescriptor extends UsbBaseNode {
+  constructor( rawData ) {
+    super( rawData );
+  }
+
+  bInterfaceNumber = () => {
+    const dataView = new Uint8Array( this.rawData, 2, 1 );
+    return dataView[0];
+  }
+
+  bAlternateSetting = () => {
+    const dataView = new Uint8Array( this.rawData, 3, 1 );
+    return dataView[0];
+  }
+
+  bNumEndpoints = () => {
+    const dataView = new Uint8Array( this.rawData, 4, 1 );
+    return dataView[0];
+  }
+
+  bInterfaceClass = () => {
+    const dataView = new Uint8Array( this.rawData, 5, 1 );
+    return dataView[0];
+  }
+
+  bInterfaceSubClass = () => {
+    const dataView = new Uint8Array( this.rawData, 6, 1 );
+    return dataView[0];
+  }
+
+  Verify = () => {
+    if ( this.rawData.byteLength !== 9 ) return false;
+    return true;
+  }
+
+  Print = () => {
+    return `Interface: verified=[${this.Verify()}] bLength=[${this.bLength()}] bInterfaceNumber=[${this.bInterfaceNumber()}] bAlternateSetting=[${this.bAlternateSetting()}] bInterfaceClass=[${this.bInterfaceClass()}] bInterfaceSubClass=[${this.bInterfaceSubClass()}]`;
+  }
+};
+
+const NodeFactory = ( rawData, interfaceClass, interfaceSubclass ) => {
   if( rawData.length < 2 ) {
     console.error( `Can't create node with not enough data: rawData.length=[${rawData.length}]` );
     return;
@@ -62,44 +204,27 @@ const NodeFactory = ( rawData, interfaceClass, subclass ) => {
     return;
   }
 
-  if( bDescriptorType === UsbConstants.DescriptorType.Device )
-  {
+  if( bDescriptorType === UsbConstants.DescriptorType.Device ) {
     return new DeviceDescriptor( rawData );
   }
-  // else if( bDescriptorType === UsbConstants.DescriptorType.Configuration ) {
-  // }
-  // else if( bDescriptorType === UsbConstants.DescriptorType.Interface ) {
-  // }
+  else if( bDescriptorType === UsbConstants.DescriptorType.Configuration ) {
+    return new ConfigurationDescriptor( rawData );
+  }
+  else if( bDescriptorType === UsbConstants.DescriptorType.Interface ) {
+    return new InterfaceDescriptor( rawData );
+  }
   else {
     return new UsbBaseNode( rawData );
   }
-}
+};
 
 const ParseUsbDescriptors = (rawData) =>
 {
-  for( let currentOffset = 0; currentOffset < rawData.length; )
-  {
-    // check size
-    if ( currentOffset + 2 >= rawData.length )
-    {
-      // bad data here...
-      console.log( `Dangling data: currentOffset=[${currentOffset}] rawData.length=${rawData.length}` );
-      return;
-    }
-
-    const bLength = rawData[currentOffset + 0];
-    const bDescriptorType = rawData[currentOffset + 1];
-
-    // console.log( `bLength=${bLength} bDescriptorType=${bDescriptorType}` );
-
-    const thisDescriptor = rawData.slice( currentOffset, currentOffset + bLength ); 
-    const newNode = NodeFactory( thisDescriptor );
-    newNode.Print();
-
-    currentOffset += bLength;
-
-  }
-}
+  const parsedDevice = new RootNode(rawData);
+  const output = parsedDevice.PrintTreeFromHere(0);
+  console.log( `-----------Parsed-----------` );
+  console.log( output );
+};
 
 // export default ParseUsbDescriptors;
 
--- src/UsbDescriptors/UsbConstants.js
+++ src/UsbDescriptors/UsbConstants.js
@@ -3,13 +3,56 @@ const DescriptorType = {
   Device: 0x01,
   Configuration: 0x02,
   String: 0x03,
-  Inteface: 0x04,
+  Interface: 0x04,
   Endpoint: 0x05,
   DeviceQualifier: 0x06,
   OtherSpeedConfig: 0x07,
   InterfaceAssociation: 0x0B,
 };
 
+const Class = {
+  PerInterface: 0,
+  Audio: 1,
+  Communications: 2,
+  HID: 3,
+  Physical: 5,
+  StillImage: 6,
+  Printer: 7,
+  MassStorage: 8,
+  Hub: 9,
+  CDC: 0x0a,
+  CSCID: 0x0b,
+  ContentSec: 0x0d,
+  Video: 0x0e,
+  WirelessController: 0xe0,
+  Miscellaneous: 0xef,
+  ApplicationSpecific: 0xfe,
+  VendorSpecific: 0xff,
+};
+
+const Uvc11 = {
+  // USB_Video_Class_1.1.pdf
+  DescriptorType: {
+    // UVC 1.1 -- Page 122
+    Undefined: 0x20,
+    Device: 0x21,
+    Configuration: 0x22,
+    String: 0x23,
+    Interface: 0x24,
+    Endpoint: 0x25,
+  },
+  Class: 0x0E,
+  Subclass: {
+    // UVC 1.1 -- Page 122
+    Undefined: 0x00,
+    VideoControl: 0x01,
+    VideoStreaming: 0x02,
+    VideoInterfaceCollection: 0x03,
+  }
+};
+
 module.exports = Object.freeze({
   DescriptorType: DescriptorType,
+  Class: Class,
+  Uvc11: Uvc11,
 });
